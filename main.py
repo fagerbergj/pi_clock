e25e6d05a782f683993f01a4fde76292d2506ec5
Jason Fagerberg <fagerbergj1@wit.edu>
2018-04-11 18:38:27 -0400

    simpler, cleaner, and better full screen mode

--- main.py
+++ main.py
@@ -4,16 +4,12 @@ from app import App
 class FullScreenApp(object):
     def __init__(self, master, **kwargs):
         self.master=master
-        self._geom='200x200+0+0'
-        master.geometry("{0}x{1}+0+0".format(
-            master.winfo_screenwidth(), master.winfo_screenheight()))
-        master.bind('<Escape>',self.resize)   
+        self.master.attributes("-fullscreen", True)
+        root.attributes("-topmost", True)
+        self.master.bind('<Escape>',self.resize)   
 
     def resize(self,event):
-        geom=self.master.winfo_geometry()
-        print(geom,self._geom)
-        self.master.geometry(self._geom)
-        self._geom=geom
+        self.master.attributes("-fullscreen", False)
 
 root=App()
 app=FullScreenApp(root)
